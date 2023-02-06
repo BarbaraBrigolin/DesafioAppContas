3ec1e6ae9a98f18c0156bbe5252c24e5d9e42063
Barbara Brigolin <barbarabrigolin@hotmail.com>
2023-02-06 17:59:43 -0300

    importei amamofire

---
+++ DesafioAppContas/Pods/Target Support Files/Pods-DesafioAppContas-DesafioAppContasUITests/Pods-DesafioAppContas-DesafioAppContasUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DesafioAppContas_DesafioAppContasUITests : NSObject
+@end
+@implementation PodsDummy_Pods_DesafioAppContas_DesafioAppContasUITests
+@end
